e0359ab1f3a60731368a4783f8b9a7750e54edec
bruno <71868455+br-re@users.noreply.github.com>
2020-10-05 00:47:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using System;
+
+
+namespace Tajmer_Za_mlijeko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=10;i>=0;i--){
+                if(i==0){
+                    Console.WriteLine("Mlijeko je gotovo");
+                    break;
+                }
+                else{
+                    Console.WriteLine($"Mlijeko ce biti gotovo za {i} sekundi");
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
